91e7e96f4ec7aade9476c000ab7fabe840012bc7
mpw <x@mpw.sh>
2024-01-30 16:08:23 -0300

    add new endpoints, new and benchmark

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ async fn main() -> std::io::Result<()> {
         .unwrap_or("4".to_string())
         .parse::<usize>()
         .unwrap();
+    let token = std::env::var("ADMIN_TOKEN").unwrap_or("supersecret".to_string());
     let db_path = std::env::var("DATABABASE_PATH").unwrap_or("./rocksdb".to_string());
     let log_level = std::env::var("LOG_LEVEL").unwrap_or("info".to_string());
     let db: kv::RocksDB = kv::KVStore::init(&db_path);
@@ -30,18 +31,22 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(db.clone()))
+            .app_data(Data::new(token.clone()))
             .app_data(JsonConfig::default().limit(1024 * 1024 * 50)) // 50 MB
             .app_data(PayloadConfig::new(1024 * 1024 * 50))
             .wrap(Logger::default())
             .service(
-                scope("/api").service(
-                    resource("/{key}")
-                        .route(get().to(kv_handler::get))
-                        .route(head().to(kv_handler::head))
-                        .route(post().to(kv_handler::post))
-                        .route(delete().to(kv_handler::delete)),
-                ),
+                scope("/api")
+                    .service(
+                        resource("/{key}")
+                            .route(get().to(kv_handler::get))
+                            .route(head().to(kv_handler::head))
+                            .route(post().to(kv_handler::post))
+                            .route(delete().to(kv_handler::delete)),
+                    )
+                    .service(resource("").route(post().to(kv_handler::new))),
             )
+            .service(resource("/benchmark").route(post().to(kv_handler::benchmark)))
     })
     .bind(("0.0.0.0", port))?
     .workers(workers)
